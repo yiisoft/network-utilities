3bdf8eac53e9dbc0ae6fd3a76f405f620332c781
Somogyi Márton <kiss.albert.marton@wwdh.hu>
2019-10-25 10:33:17 +0200

    exception message fix

--- src/IpHelper.php
+++ src/IpHelper.php
@@ -88,7 +88,7 @@ class IpHelper
             if (@inet_pton('::1') === false) {
                 throw new \RuntimeException('IPv6 is not supported by inet_pton()!');
             }
-            throw new \InvalidArgumentException('Invalid or not supported IP format by inet_pton()!');
+            throw new \InvalidArgumentException("Unrecognized address $ip");
         }
         $hex = unpack('H*hex', $ipRaw);
         return substr(preg_replace('/([a-f0-9]{4})/i', '$1:', $hex['hex']), 0, -1);
