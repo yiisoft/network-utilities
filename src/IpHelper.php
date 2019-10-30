414f23a22fcf0259b2ed362c6ccd6efa0af78cb8
Somogyi Márton <kiss.albert.marton@wwdh.hu>
2019-10-30 06:07:24 +0100

    phpdoc #5

--- src/IpHelper.php
+++ src/IpHelper.php
@@ -8,16 +8,16 @@ class IpHelper
     public const IPV6 = 6;
 
     /**
-     * IPv4 address regexp. This pattern is PHP and JavaScript compatible.
+     * IPv4 address pattern. This pattern is PHP and JavaScript compatible.
      * Allows to define your own IP regexp eg. `'/^'.IpHelper::IPV4_PATTERN.'/(\d+)$/'`
      */
     public const IPV4_PATTERN = '((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))';
     /**
-     * IPv6 address regexp-pattern. This regexp is PHP and Javascript compatible.
+     * IPv6 address regexp. This regexp is PHP and Javascript compatible.
      */
     public const IPV4_REGEXP = '/^' . self::IPV4_PATTERN . '$/';
     /**
-     * IPv6 address regexp-pattern. This pattern is PHP and Javascript compatible.
+     * IPv6 address pattern. This pattern is PHP and Javascript compatible.
      * Allows to define your own IP regexp eg. `'/^'.IpHelper::IPV6_PATTERN.'/(\d+)$/'`
      */
     public const IPV6_PATTERN = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:' . self::IPV4_PATTERN;
