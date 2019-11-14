a917e3ed84bea378cd863d104c194e90a6aaaecb
Alexander Kartavenko <askemailbox@gmail.com>
2019-11-14 15:38:37 +0200

    Fix phan issues (#15)

--- src/DnsHelper.php
+++ src/DnsHelper.php
@@ -6,6 +6,7 @@ class DnsHelper
 {
     /**
      * @param string $hostname hostname without dot at end
+     * @return bool
      * @link https://bugs.php.net/bug.php?id=78008
      */
     public static function existsMx(string $hostname): bool
@@ -18,6 +19,7 @@ class DnsHelper
             $result = @dns_get_record($hostname, DNS_MX);
             return $result !== false && count($result) > 0;
         } catch (\Throwable $t) {
+            assert($t);
             // eg. name servers are not found https://github.com/yiisoft/yii2/issues/17602
         }
         return false;
@@ -25,6 +27,8 @@ class DnsHelper
 
     /**
      * @link https://bugs.php.net/bug.php?id=78008
+     * @param string $hostname
+     * @return bool
      */
     public static function existsA(string $hostname): bool
     {
@@ -35,6 +39,7 @@ class DnsHelper
             $result = @dns_get_record($hostname, DNS_A);
             return $result !== false && count($result) > 0;
         } catch (\Throwable $t) {
+            assert($t);
             // eg. name servers are not found https://github.com/yiisoft/yii2/issues/17602
         }
         return false;
@@ -42,11 +47,13 @@ class DnsHelper
 
     /**
      * @link https://tools.ietf.org/html/rfc5321#section-5
+     * @param string $hostnameOrEmail
+     * @return bool
      */
     public static function acceptsEmails(string $hostnameOrEmail): bool
     {
         if (strpos($hostnameOrEmail, '@') !== false) {
-            [$void, $hostnameOrEmail] = explode('@', $hostnameOrEmail, 2);
+            [, $hostnameOrEmail] = explode('@', $hostnameOrEmail, 2);
         }
         return self::existsMx($hostnameOrEmail) || self::existsA($hostnameOrEmail);
     }
