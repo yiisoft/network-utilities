cfbd49ea1d68e2056ac409ff469afe5f063b63d3
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-08-06 09:42:33 +0300

    Update src/IpHelper.php
    Co-authored-by: Alexander Makarov <sam@rmcreative.ru>

--- src/IpHelper.php
+++ src/IpHelper.php
@@ -52,7 +52,7 @@ final class IpHelper
      * @param string $ip The valid IPv4 or IPv6 address.
      * @param bool $validate Enable perform IP address validation. False is best practice if the data comes from a trusted source.
      *
-     * @return int Value of {@see IPV4} or {@see IPV6} constants.
+     * @return int Value of either {@see IPV4} or {@see IPV6} constant.
      */
     public static function getIpVersion(string $ip, bool $validate = true): int
     {
